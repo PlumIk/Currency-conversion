be5aa2eab7572f1e7537bfcfe608bac71fef418d
chema <zmey25062000@gmail.com>
2022-04-23 12:35:06 +0700

    Добавлены графические элементы

---
+++ Currency conversion/ViewModel/MainViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Currency_conversion
+{
+    public abstract class MainViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
