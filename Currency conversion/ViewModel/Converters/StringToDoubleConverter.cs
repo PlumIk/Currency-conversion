dc867bce2e1e324277b45cecbb2777f077d70c5b
chema <zmey25062000@gmail.com>
2022-04-23 14:05:22 +0700

    Добавлен конвектор для строк и double

---
+++ Currency conversion/ViewModel/Converters/StringToDoubleConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Currency_conversion.ViewModel.Converters
+{
+    internal class StringToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string one = (string)value;
+            double ret;
+            if( Double.TryParse(one, out ret))
+            {
+                return ret;
+            }
+            return -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            double one = (double)value;
+            if(one < 0)
+            {
+                return "Невозможно перевести";
+            }
+            return one.ToString();
+        }
+    }
+}
