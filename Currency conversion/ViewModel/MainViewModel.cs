2d43ae0afbe9ef59b3cffa22cc63cb35d08d0910
chema <zmey25062000@gmail.com>
2022-04-23 14:04:36 +0700

    Добавлена MV

---
+++ Currency conversion/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_conversion
+{
+    internal class MainViewModel : MainViewModelBase
+    {
+        private string _value1;
+        private string _value2;
+        public string Value1
+        {
+            get
+            {
+                return _value1;
+            }
+            set
+            {
+                if (_value1 != value)
+                {
+                    _value1 = value;
+                }
+            }
+        }
+
+        public string Value2
+        {
+            get
+            {
+                return _value2;
+            }
+            set
+            {
+                if (_value2 != value)
+                {
+                    _value2 = value;
+                }
+            }
+        }
+    }
+}
