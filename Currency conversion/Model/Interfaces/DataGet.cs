32b40412adbd9ebd56f6ddc95146e52d14e8fe85
chema <zmey25062000@gmail.com>
2022-04-24 13:01:45 +0700

    Определения интерфейса для получения данных

---
+++ Currency conversion/Model/Interfaces/DataGet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_conversion
+{
+    public interface DataGet
+    {
+        List<string> GetNames();
+        Dictionary<string, double> GetValues();
+    }
+}
